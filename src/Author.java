a6f1bf487bc586f21305d8235b8d92356ad70f8e
chasehammock <chasehammock@gmail.com>
2017-10-03 09:22:59 -0500

    finished the better-fix branch and committing the working code minus the suppressed warnings

--- src/Author.java
+++ src/Author.java
@@ -5,31 +5,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
+
     public List<String> getBooks() {
         return books;
     }
 
+
     public List<String> publishedBooks() {
         return books;
     }
+
 
     public void addBook(String book) {
         books.add(book);
     }
 
     @Override
-    public String sortName() {
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
